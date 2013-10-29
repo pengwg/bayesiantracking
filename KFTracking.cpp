1f99b79477dd76d676d1d6762b51e00785344e9c
Peng Wang <pengwg@gmail.com>
2013-10-29 15:35:46 -0500

    Optimize parameter

--- KFTracking.cpp
+++ KFTracking.cpp
@@ -61,8 +61,8 @@ void CKFTracking::OnTransitionJacobian(KFMatrix_VxV  &F) const
 
 void CKFTracking::OnTransitionNoise(KFMatrix_VxV &Q) const
 {
-    Q(0,0) = Q(1,1) = Q(4,4) =
-    Q(2,2) = Q(3,3) = square( TRANSITION_MODEL_STD_XY );
+    Q(0,0) = Q(1,1) = square( TRANSITION_MODEL_STD_ANGLE );
+    Q(2,2) = Q(4,4) = Q(3,3) = square( TRANSITION_MODEL_STD_XY );
 }
 
 void CKFTracking::OnGetObservationNoise(KFMatrix_OxO &R) const
